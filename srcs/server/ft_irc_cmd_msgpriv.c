357cce66dbb5a8284c3bc0531c7cbc602448d28c
Guillaume TORRESANI <gtorresa@student.42.fr>
2017-11-10 17:24:15 +0100

    Fix display return privmsg

--- srcs/server/ft_irc_cmd_msgpriv.c
+++ srcs/server/ft_irc_cmd_msgpriv.c
@@ -6,18 +6,18 @@
 /*   By: gtorresa <gtorresa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/09 20:06:23 by gtorresa          #+#    #+#             */
-/*   Updated: 2017/11/09 22:09:19 by gtorresa         ###   ########.fr       */
+/*   Updated: 2017/11/10 17:16:15 by gtorresa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_irc_server.h"
 
-static void	ft_irc_cmd_msgpriv_fd(t_env *e, int cs, t_privmsg *msg)
+static void	ft_irc_cmd_msgpriv_fd(t_env *e, int cs, t_privmsg *msg, int send)
 {
 	char		buff[1024];
 
 	ft_bzero(buff, 1024);
-	ft_irc_print_header_msg(&buff[0], e, cs);
+	ft_irc_print_header_msg(&buff[0], e, send);
 	ft_strcat(buff, "PRIVMSG ");
 	ft_strcat(buff, msg->dest);
 	ft_strcat(buff, " :");
@@ -35,7 +35,7 @@ int			ft_irc_cmd_msgpriv(t_env *e, int cs, t_privmsg *msg)
 	{
 		if (ft_strcmp(e->fds[i].username, msg->dest) == 0)
 		{
-			ft_irc_cmd_msgpriv_fd(e, i, msg);
+			ft_irc_cmd_msgpriv_fd(e, i, msg, cs);
 			free(msg);
 			return (0);
 		}
